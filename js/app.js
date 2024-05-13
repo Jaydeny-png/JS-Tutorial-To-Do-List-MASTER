aa5a235358d461a960862e1c4a312fbbd838afe1
Jaydeny-png <jyau1020@gmail.com>
2024-05-13 10:43:36 -0400

    day 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+
+const dateElement = document.getElementById("date");
+
+
+
+const list = document.getElementById("list");
+function addToDo(toDo) {
+    const text = `<li class="item">
+<i class="co fa fa-circle-thin" job="complete"></i>
+<p class="text">Drink Coffee</p>
+<i class="de fa fa-trash-o" job="delete"></i>
+</li>`
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
+addToDo("Drink Coffee");
+
+
+
+const input = document.getElementById("input");
+document.addEventListener("Keyup",function(event){
+if(event.keyCode == 13){
+    const toDo = input.value;
+    if(toDo){
+        addToDo(toDo);
+    }
+    input.value = "";
+}
+});
