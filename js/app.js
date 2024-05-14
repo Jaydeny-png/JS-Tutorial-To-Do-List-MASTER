48319beac06b542b921398ed8d60e5c9f673fd58
Jaydeny-png <jyau1020@gmail.com>
2024-05-14 11:51:51 -0400

    working hard or hardly working

--- js/app.js
+++ js/app.js
@@ -1,30 +1,28 @@
 const clear = document.querySelector(".clear");
-
 const dateElement = document.getElementById("date");
-
-
-
 const list = document.getElementById("list");
-function addToDo(toDo) {
-    const text = `<li class="item">
-<i class="co fa fa-circle-thin" job="complete"></i>
-<p class="text">Drink Coffee</p>
-<i class="de fa fa-trash-o" job="delete"></i>
-</li>`
-    const position = "beforeend";
-    list.insertAdjacentHTML(position, text);
-}
-addToDo("Drink Coffee");
-
-
-
 const input = document.getElementById("input");
-document.addEventListener("Keyup",function(event){
-if(event.keyCode == 13){
-    const toDo = input.value;
-    if(toDo){
-        addToDo(toDo);
-    }
-    input.value = "";
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+function addToDo(toDo){
+
+    const item = `
+    <i class="fa fa-circle-thin co" job="compete" id="0"></i>
+    <p class="text">${toDo}</p>
+    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
 }
-});
+
+addToDo("drink coffee");
